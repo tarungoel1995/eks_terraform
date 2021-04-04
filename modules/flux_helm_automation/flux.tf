cec473b339b23e1f28883fb21967905a3092138a
lovedeepsh <lovedeeps789@gmail.com>
2021-04-04 22:42:22 +0400

    Auomated the process of flux deploy and helm deploy

---
+++ modules/flux_helm_automation/flux.tf
@@ -0,0 +1,50 @@
+resource "helm_release" "flux" {
+    depends_on = [kubernetes_namespace.flux-ns]
+    name        = "flux" #format("helm_operator-%s", var.environment)
+    repository  = "https://charts.fluxcd.io"
+    chart       = "flux"
+    version     = "1.3.0"
+    namespace   = var.namespace
+
+    values = [
+        file("values/flux.yaml")
+    ]
+
+    set {
+        name    = "git.ssh.known_hosts"
+        value   = local.github_known_hosts
+    }
+
+    set {
+        name    = "git.ssh.secretName"
+        value   = kubernetes_secret.git_ssh.metadata.0.name
+    }
+
+    set {
+        name    = "git.branch"
+        value   = var.branch
+    }
+
+    #set {
+    #    name    = "kube.config"
+    #    value   = yamlencode({
+    #        apiVersion  : "v1"
+    #        clusters    : []
+    #        contexts    :   [
+    #            {
+    #                context : {
+    #                    cluster : ""
+    #                    namespace   : var.namespace
+    #                    user    : ""
+    #                }
+    #                name    : "default"
+    #            }
+    #        ]
+    #        current-context : "default"
+    #        kind            : "Config"
+    #        preferences     : {}
+    #        users           : []
+    #    })
+    #}
+
+}
