4328a985df2919ef8947fc463195ba586abed085
lovedeepsh <lovedeeps789@gmail.com>
2021-04-10 16:46:22 +0400

    Added Makefiles, Corrected Bastion security group and Corrected force deletion of S3 bucket

--- modules/infra/bastion.tf
+++ modules/infra/bastion.tf
@@ -3,6 +3,14 @@ resource "aws_security_group" "eks_bastion_sg" {
   description = "Security group for bastion in the cluster"
   vpc_id      = aws_vpc.main_eks_istio.id
 
+  ingress {
+    description = "Allow node to communicate with all IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -13,16 +21,6 @@ resource "aws_security_group" "eks_bastion_sg" {
   tags = {
     Creator = "terraform"
   }
-}
-
-resource "aws_security_group_rule" "eks-bastion-ingress" {
-  description              = "Allow node to communicate with all IP"
-  from_port                = 22
-  protocol                 = "-1"
-  cidr_blocks              = ["0.0.0.0/0"]
-  to_port                  = 22
-  security_group_id        = aws_security_group.eks_bastion_sg.id
-  type                     = "ingress"
 }
 
 resource "aws_instance" "bastion" {
